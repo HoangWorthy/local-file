da51d25d9b5777fff6b96c7000cccd4906124054
HoangWorthy <Hoangsusp@gmail.com>
2023-12-07 21:32:17 +0700

    asda

--- C/Bai1.c
+++ C/Bai1.c
@@ -5,12 +5,12 @@
 int main(int argc, char** argv) {
 	int a,b,c;
     float x1,x2;
-    scanf("%d\n%d\n%d\n",a,b,c);
-    int delta = sqr(b)- 4*a*c;
+    scanf("%d\n%d\n%d",&a,&b,&c);
+    int delta = (b*b)- 4*a*c;
     if (delta > 0) {
         x1 = (-b + sqrt(delta))/(2*a);
         x2 = (-b - sqrt(delta))/(2*a);
-        printf("%f\n%f",x1,x2);
+        printf("x1 = %f\nx2 =%f",x1,x2);
     }
     else if (delta == 0) {
         x1 = -b /(2*a);
